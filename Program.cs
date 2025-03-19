9a572f6985f7e04242242f5f84ac8b3026d42ce3
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-19 12:04:29 +0300

    DivideByZeroException

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+try
+{
+    int number1 = 10;
+    int number2 = 0;
+    int result = number1 / number2;
+    Console.WriteLine("Result: " + result);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("A Division by zero erroe accurred!");
+    Console.WriteLine("Error message: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("The finally block is executed in all cases!");
+}
